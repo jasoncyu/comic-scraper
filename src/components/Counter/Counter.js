fa217fe78a8a1c42b45bf0b0c5caf4552e0b94a8
jasoncyu <yujason2@gmail.com>
2016-05-12 17:49:46 -0700

    Add incr/decr.

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,12 +1,27 @@
 import React, { Component, PropTypes } from 'react';
+import { createStore } from 'redux'
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Counter.scss'
 
+// Start with default if no state is passed
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
 class Counter extends Component {
-  render() {
+  render(value, onIncrement, onDecrement) {
     return (
       <div>
-        Counter
+        <div>{value}</div>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
       </div>
     )
   }
--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -7,26 +7,49 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { PropTypes } from 'react';
+import React, {
+  PropTypes
+} from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import { createStore } from 'redux'
 import s from './Contact.scss';
 import Counter from '../../components/Counter'
 
 const title = 'Contact Us';
 
+const store = createStore(counter)
+
 function Contact(props, context) {
   context.setTitle(title);
+  const onIncrement = () => {
+    store.dispatch({
+      type: 'INCREMENT',
+    })
+  }
+
+  const onDecrement = () => {
+    store.dispatch({
+      type: 'DECREMENT',
+    })
+  }
+
   return (
     <div className={s.root}>
       <div className={s.container}>
         <h1>{title}</h1>
         <p>...</p>
-        <Counter />
+        <Counter
+          value={store.getState()}
+          onIncrement={onIncrement}
+          onDecrement={onDecrement}
+        />
       </div>
     </div>
   );
 }
 
-Contact.contextTypes = { setTitle: PropTypes.func.isRequired };
+Contact.contextTypes = {
+  setTitle: PropTypes.func.isRequired,
+};
 
 export default withStyles(s)(Contact);
