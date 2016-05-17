d230ce01d87d263a0b9237e1aa731a2c86d371f3
jasoncyu <yujason2@gmail.com>
2016-05-16 17:29:19 -0700

    WIP

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,15 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import { createStore } from 'redux'
+import { connect } from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Counter.scss'
+import { increment, decrement } from '../../actions/counter'
 
-function Counter(props, context) {
-  console.log("context.getState(): ", context.store.getState());
+function Counter({ counterValue, onIncrement, onDecrement }, context) {
   return (
     <div>
-      <div>asdfasdf</div>
-      <button onClick={props.onIncrement}>+</button>
-      <button onClick={props.onDecrement}>-</button>
+      <div>counterValue</div>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
     </div>
   )
 }
@@ -17,4 +18,8 @@ Counter.contextTypes = {
   store: PropTypes.object.isRequired,
 }
 
-export default withStyles(s)(Counter);
+export default connect(state => ({
+  counterValue: state.counterValue,
+}), {
+  increment, decrement,
+})(withStyles(s)(Counter));
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -2,18 +2,19 @@ import { INCREMENT, DECREMENT } from '../constants'
 
 export default function counter(state = {}, action) {
   console.log("action.tpye: ", action.type);
+  console.log("state: ", state);
   switch (action.type) {
     case INCREMENT:
       console.log('process increment');
       return {
         ...state,
-        counter: state.counter + 1
+        counterValue: state.counterValue + 1
       }
     case DECREMENT:
       console.log('process decrement');
       return {
         ...state,
-        counter: state.counter - 1
+        counterValue: state.counterValue - 1
       }
     default:
       console.log('process default action');
