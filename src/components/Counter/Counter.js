645c73fbdb00d024a69fb3dad32b6efadc66d92b
jasoncyu <yujason2@gmail.com>
2016-05-16 19:22:23 -0700

    Remove unneeded imports.

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,5 +1,4 @@
-import React, { Component, PropTypes } from 'react';
-import { createStore } from 'redux'
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Counter.scss'
@@ -30,6 +29,7 @@ const mapStateToProps = (state) => {
     counterValue: state.counter.counterValue,
   }
 }
+
 const mapDispatchToProps = { onIncrement: increment, onDecrement: decrement }
 
 export default connect(mapStateToProps, mapDispatchToProps)(withStyles(s)(Counter));
